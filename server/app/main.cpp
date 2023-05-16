118e5cbb3fa5e622330c97e3f39943ddf84ef1d5
metaconscious <justmarkoven@gmail.com>
2023-05-16 15:32:31 +0800

    impl: synchronized one-time server reader

--- server/app/main.cpp
+++ server/app/main.cpp
@@ -1,7 +1,25 @@
+#include <asio.hpp>
+#include <cstdlib>
 #include <iostream>
 
 int main()
 {
+    asio::io_context ioContext{};
+    asio::ip::tcp::endpoint endpoint{ asio::ip::tcp::v4(), 12345 };
+    asio::ip::tcp::acceptor acceptor{ ioContext, endpoint };
+    auto socket{ acceptor.accept() };
 
-    return 0;
+    char buffer[1024];
+    asio::error_code errorCode{};
+    socket.read_some(asio::buffer(buffer), errorCode);
+
+    if (errorCode)
+    {
+        std::cerr << "Error while reading: " << errorCode.message() << '\n';
+        return EXIT_FAILURE;
+    }
+
+    std::cout << std::string_view{ buffer } << '\n';
+
+    return EXIT_SUCCESS;
 }
